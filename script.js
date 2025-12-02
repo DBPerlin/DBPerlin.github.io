96601a4d2e67cc9cae06d02c5049cefdcc18efab
DBPerlin <perlinmanuella@gmail.com>
2025-12-02 20:47:44 -0300

    Removing unused function

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-function sendMail(params){
-    var tempParams = {
-        from_name: document.getElementById("fromName").value,
-        email_sender: document.getElementById("emailSender").value,
-        subject_sender: document.getElementById("subjectSender").value,
-        message_sender: document.getElementById("message").value,
-    };
-    emailjs.send('service_3uydnjq','service_3uydnjq',tempParams)
-     .then(function(res){
-        Swal.fire({
-            position: 'center',
-            icon: 'success',
-            title: 'Mail Sent Successfully!',
-            showConfirmButton: false,
-            timer: 1500,
-          });
-     });
-}
 
 $(document).ready(function () {
     $(window).scroll(function () {
